bd4e7a190274b86f4475a2d113491d606ce81f46
Jan Kwinta <jankwinta@icloud.com>
2025-10-07 20:08:09 +0200

    kwadrat

--- Program0/script.js
+++ Program0/script.js
@@ -2,15 +2,17 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 // Punkty kwadratu
-const x1 = 200, y1 = 50;   // góra
-const x2 = 100, y2 = 300;  // lewy dół
-const x3 = 300, y3 = 300;  // prawy dół
+const x1 = 200, y1 = 200;
+const x2 = -200, y2 = 200;
+const x3 = 200, y3 = -200;
+const x4 = -200, y4 = -200;
 
 // Rysowanie sciezki
 ctx.beginPath();
 ctx.moveTo(x1, y1);
 ctx.lineTo(x2, y2);
 ctx.lineTo(x3, y3);
+ctx.lineTo(x4, y4);
 ctx.closePath();
 
 // Obrysowanie
